837f24a06db827a1b81f70a25f80de89aaccff40
YUKI240306 <24_yukv@liceum.ztu.edu.ua>
2025-04-21 09:49:40 +0300

    css updated.

--- js/script.js
+++ js/script.js
@@ -12,11 +12,28 @@
       kDiv.innerHTML = `
         
         <div class = "k-container">
+
           
           <h3 class = "k-name">${k.name}</h3>
           <img class = "U" src='img/${index + 1}.jpeg' alt="${k.name}" onerror="this.onerror=null;this.src='#';" />
-          <p class = "pricee">${k.price}грн</p>
-          <p class = "sale_price">${k.sale_price}грн</p>
+
+          <div class = "pictures">
+          <div class = "paw><img class = "paws" src='img/paws.png' alt="paw" onerror="this.onerror=null;this.src='#';" /></div>
+          <div class = "circul>"<img class = "paws" src='#' alt="paw" onerror="this.onerror=null;this.src='#';" /></div>
+
+          </div>
+
+          <div class="price-block">
+            <p class="old-price">${k.pricer}грн</p>
+            <p class="new-price">${k.sale_price}грн</p>
+          </div>
+
+          <div class="coupon-price">
+            <span>ціна за купоном</span>
+            <span class="price">${k.price}грн</span>
+          </div>
+
+          
         
         </div>
       `;
